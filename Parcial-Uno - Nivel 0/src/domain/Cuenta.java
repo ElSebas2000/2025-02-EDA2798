28755ac113df62bccfd18672185de76f5eaeff5e
ElSebas2000 <167447104+ElSebas2000@users.noreply.github.com>
2025-09-12 11:47:18 -0500

    Update Cuenta.java

--- Parcial-Uno - Nivel 0/src/domain/Cuenta.java
+++ Parcial-Uno - Nivel 0/src/domain/Cuenta.java
@@ -5,10 +5,49 @@ public class Cuenta {
     private long dniCliente;
     private double saldoActual;
 
+    // 🔹 Constructor vacío
+    public Cuenta() {
+    }
+
+    // 🔹 Constructor con parámetros
     public Cuenta(String numeroCuenta, long dniCliente, double saldoActual) {
         this.numeroCuenta = numeroCuenta;
         this.dniCliente = dniCliente;
         this.saldoActual = saldoActual;
-    }  
+    }
 
+    // 🔹 Getters y setters
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public long getDniCliente() {
+        return dniCliente;
+    }
+
+    public void setDniCliente(long dniCliente) {
+        this.dniCliente = dniCliente;
+    }
+
+    public double getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+
+    // 🔹 toString
+    @Override
+    public String toString() {
+        return "Cuenta{" +
+                "numeroCuenta='" + numeroCuenta + '\'' +
+                ", dniCliente=" + dniCliente +
+                ", saldoActual=" + saldoActual +
+                '}';
+    }
 }
