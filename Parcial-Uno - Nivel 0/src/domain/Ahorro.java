dbf1fbddcfcdddcf4942e86b5329c6516eb6b264
ElSebas2000 <167447104+ElSebas2000@users.noreply.github.com>
2025-09-12 11:46:28 -0500

    Update Ahorro.java

--- Parcial-Uno - Nivel 0/src/domain/Ahorro.java
+++ Parcial-Uno - Nivel 0/src/domain/Ahorro.java
@@ -1,13 +1,36 @@
 package domain;
 
-public class Ahorro extends Cuenta{
+public class Ahorro extends Cuenta {
     private String fechaCreacion;
 
+    // 🔹 Constructor vacío
+    public Ahorro() {
+        super();
+    }
 
+    // 🔹 Constructor con parámetros
     public Ahorro(String numeroCuenta, long dniCliente, double saldoActual, String fechaCreacion) {
         super(numeroCuenta, dniCliente, saldoActual);
         this.fechaCreacion = fechaCreacion;
     }
 
-    
+    // 🔹 Getter y Setter
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    // 🔹 toString
+    @Override
+    public String toString() {
+        return "Ahorro{" +
+                "numeroCuenta='" + getNumeroCuenta() + '\'' +
+                ", dniCliente=" + getDniCliente() +
+                ", saldoActual=" + getSaldoActual() +
+                ", fechaCreacion='" + fechaCreacion + '\'' +
+                '}';
+    }
 }
